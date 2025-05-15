7cb244163d2e223c49cc1a891224d4b112aff540
Renata Lima <64491568+renatadrake@users.noreply.github.com>
2025-05-15 16:35:29 -0300

    inclusao das novas validacoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,14 +33,16 @@ function iniciarJogoDocumentos() {
         }
 
         if (acertosNaRodada === corretosPorRodada[rodadaAtual]) {
+          desativarDocumentosVisiveis();
+
           if (rodadaAtual < totalRodadas) {
             setTimeout(() => {
               $("#modalNovaRodada").modal("show");
               setTimeout(() => {
                 $("#modalNovaRodada").modal("hide");
                 avancarRodada();
-              }, 5000);
-            }, 900);
+              }, 2000);
+            }, 500);
           } else {
             setTimeout(() => {
               avancarRodada();
@@ -86,6 +88,13 @@ function avancarRodada() {
   });
 }
 
+function desativarDocumentosVisiveis() {
+  $(".btn-doc:visible").draggable("disable").css({
+    "pointer-events": "none",
+    opacity: "0.5",
+  });
+}
+
 function ativarDragNosItensVisiveis() {
   $(".btn-doc:visible").draggable({
     containment: ".tela-1",
@@ -102,53 +111,10 @@ function abreResposta() {
   });
 }
 
-// function escalaProporcao(largura, altura) {
-//   var larguraScreen = $(window).width();
-//   var alturaScreen = $(window).height();
-//   var proporcaoAltura = (alturaScreen * 100) / altura;
-//   var proporcaoLargura = (larguraScreen * 100) / largura;
-//   var proporcao, larguraAltura, larguraAlturaAuto;
-
-//   if (proporcaoAltura < proporcaoLargura) {
-//     larguraAltura = "height";
-//     larguraAlturaAuto = "width";
-//     proporcao = proporcaoAltura / 100;
-//   } else {
-//     larguraAltura = "width";
-//     larguraAlturaAuto = "height";
-//     proporcao = proporcaoLargura / 100;
-//   }
-
-//   console.log(proporcao, proporcaoAltura, proporcaoLargura);
-//   return [proporcao, larguraAltura, larguraAlturaAuto];
-// }
-
-// function resizeBodyConteudo() {
-//   var proporcao1920 = escalaProporcao(1920, 1080)[0];
-
-//   $(".conteudo").css({
-//     transform: "scale(" + proporcao1920 + ")",
-//     "transform-origin": "center center",
-//   });
-
-//   var proporcao900;
-
-//   if ($(window).width() < 992) {
-//     proporcao900 = escalaProporcao(900, 576)[0];
-//   } else {
-//     proporcao900 = 1;
-//   }
-// }
-
 $(window).on("load", function () {
   $("#modalIntroducao").modal("show");
 
   iniciarJogoDocumentos();
   ativarDragNosItensVisiveis();
   abreResposta();
-
-  // resizeBodyConteudo();
-  // $(window).resize(function () {
-  //   resizeBodyConteudo();
-  // });
 });
